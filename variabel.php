c7e87dd43d6aacbe7203d38d7f1907e8f050d79c
produktif <devitafauziah07@gmail.com>
2024-02-01 08:09:59 +0700

    first upload

---
+++ variabel.php
@@ -0,0 +1,12 @@
+<?php
+$nim="E32232338";
+$nama="Devita";
+$prodi="Teknik Komputer";
+echo "Selamat Praktikum";
+echo "<br/>";
+echo $nim;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+echo $prodi;
+?>
